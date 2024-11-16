e74327e0000a7aee9f1b0fab55e0e990a0dfd747
Vladimir Filipovic <vladofilipovic@hotmail.com>
2024-11-16 09:27:06 +0100

    popravio

--- 03-01-linearno programiranje/07-chairs-tables/chairs_tables.py
+++ 03-01-linearno programiranje/07-chairs-tables/chairs_tables.py
@@ -1,19 +1,19 @@
 from linopy import Model
 
-m = Model()
+model = Model()
 
-c = m.add_variables(lower=0, name="Number of chairs")
-t = m.add_variables(lower=0, name="Number of tables")
+c = model.add_variables(lower=0, name="Number of chairs")
+t = model.add_variables(lower=0, name="Number of tables")
 
-m.add_constraints(5*c + 20*t <= 400, name="Mahogany constraint")
-m.add_constraints(10*c + 15*t <= 450, name="Resources harnessing constraint")
+model.add_constraints(5*c + 20*t <= 400, name="Mahogany constraint")
+model.add_constraints(10*c + 15*t <= 450, name="Resources harnessing constraint")
 
 
-m.add_objective(45*c + 80*t, sense='max')
+model.add_objective(45*c + 80*t, sense='max')
 
-print(m)
+print(model)
 
-m.solve()
+model.solve()
 
 print("{}:{}\n".format(c, c.solution))
 print("{}:{}\n".format(t, t.solution))
